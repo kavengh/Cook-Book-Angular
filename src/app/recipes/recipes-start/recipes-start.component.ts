4f9ecb30ab216297b86e0ba556005a760b5c62e7
Kaven Vohra <vohrakaven@gmail.com>
2022-04-07 16:30:16 -0400

    new recipe default component

---
+++ src/app/recipes/recipes-start/recipes-start.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-start',
+  templateUrl: './recipes-start.component.html',
+  styleUrls: ['./recipes-start.component.scss'],
+})
+export class RecipesStartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
